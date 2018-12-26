5d8e8146e9a1c7ec75230da39bd598dcf04fdf7a
arshad <mdarshadrashid98@gmail.com>
2018-12-26 16:20:15 +0530

    header

---
+++ mywebsite.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/d1.css">
+        <title>hotel</title>
+    </head>
+    <body>
+        <div class="header">
+            <div class="h1">
+
+                <div class="h11">
+                    <b>AAA</b> &nbsp;&nbsp;<span class="logo_name">india's largest bookings web's;</span>
+                    <br><br>
+                    <span>(athar,arshad,arham)</span>
+                </div>
+                <div class="h12">
+                    <img src="pic/3a.jpeg" class="hp1">
+                </div>
+                <div class="h11" id='h1text'> 
+                    <img src="pic/l3icon.png"> &nbsp;<a href="login.php" target="blanck">login</a>&Backslash;&nbsp;<a href="signup.php">sign up</a> &nbsp;&nbsp;<img src="pic/w3icon.png">&nbsp;contact:8271865539
+                </div>
+
+            </div>
+            <div class="h2"></div>
+            <div class="h3"><img src="pic/room1.jpg"></div>
+            <div class="h4"></div>
+        </div>
+        <div class="middle"></div>
+        <div class="fotter"></div>
+    </body>
+</html>
